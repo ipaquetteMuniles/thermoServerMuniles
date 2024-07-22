3bed61d8fd1d8356081e69c7cf46ac62e1406fa6
ipaquetteMuniles <ipaquette@muniles.ca>
2024-07-22 09:51:37 -0300

    Add files via upload

--- CollectData.py
+++ CollectData.py
@@ -8,17 +8,18 @@ Iohann Paquette
 Bibliothèques
 """
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 from pyhtcc import PyHTCC
 import os
 import schedule
 import threading
 import firebase_admin
-from firebase_admin import credentials,db
+from firebase_admin import credentials, db
 import json
 import pytz
 import requests
 import pandas as pd
+
 """
 Constantes
 """
@@ -27,22 +28,18 @@ DATABASE_URL = 'https://appmuniles-default-rtdb.firebaseio.com/'
 
 cred = credentials.Certificate("./appmuniles-firebase-adminsdk-qp9zl-b94ab152f3.json")
 default_app = firebase_admin.initialize_app(cred, {
-	'databaseURL':DATABASE_URL
+    'databaseURL': DATABASE_URL
 })
 
 LONGITUDE = -61.750022
 LATITUDE = 47.445642
 TIMEZONE = 'America/Goose_Bay'
-RELOG_TIME = 3600 # en seconde - 1h
+RELOG_TIME = 7200  # en secondes - 2h
 
 """
 Classe
 """
 class Collector:
-    """
-    Fonctions
-    """
-
     """
     Constructeur de notre classe
     """
@@ -50,29 +47,43 @@ class Collector:
         self.user = None
         self.email = None
         self.mdp = None
-
-        #authentification
+        self.timer = None
+        # Authentification
         self.login()
 
         self.zones = self.user.get_all_zones()
         self.timeRunning = None
         self.running = True
+        self.list_threads = []
 
     def login(self):
         try:
-            print('Authentification...')
-
             if self.email is None or self.mdp is None:
                 self.email = input('Votre courriel : ')
                 self.mdp = input('Mot de passe : ')
-                # Authentification
-                self.user = PyHTCC(self.email, self.mdp)
-            else:
-                self.user.authenticate()
+
+            self.user = PyHTCC(self.email, self.mdp)
+            self.user.authenticate()
+
+            print('Authentification réussie.')
+            self.start_timer()
+
         except Exception as e:
             print(e)
             with open("error_log.txt", "a") as file:
                 file.write(f"Error while login: {str(e)}\n")
+            self.retry_login()
+
+    def retry_login(self):
+        print('Réessayer l\'authentification...')
+        time.sleep(5)
+        self.login()
+
+    def start_timer(self):
+        if self.timer:
+            self.timer.cancel()
+        self.timer = threading.Timer(RELOG_TIME - 300, self.login)  # Re-authenticate 5 minutes before expiry
+        self.timer.start()
 
     def run_schedule(self):
         try:
@@ -81,13 +92,11 @@ class Collector:
                 schedule.run_pending()
                 time.sleep(1)
         except Exception as e:
-            input()
             print(e)
-            print('Déconnexion...')
             self.user.logout()
+            self.cleanup_threads()
 
     def get_temperature(self):
-
         url = "https://api.open-meteo.com/v1/forecast"
         params = {
             "latitude": LATITUDE,
@@ -101,22 +110,20 @@ class Collector:
             response.raise_for_status()
             response = response.json()
 
-            # Process hourly data
             current = response['current']
             current_temperature_2m = current['temperature_2m']
             current_humidity_2m = current['relative_humidity_2m']
-            # Get the latest temperature
 
-            return (current_temperature_2m,current_humidity_2m)
+            return current_temperature_2m, current_humidity_2m
 
         except requests.RequestException as e:
+            print(e)
             with open("error_log.txt", "a") as file:
                 file.write(f"Error fetching data from Open-Meteo API: {str(e)}\n")
             return None
 
     def get_current_data(self, zone):
         zone.refresh_zone_info()
-
         zone_info = zone.zone_info
         latest_data = zone_info['latestData']
         ui_data = latest_data['uiData']
@@ -124,10 +131,9 @@ class Collector:
 
         return zone_info, ui_data, fan_data
 
-    def get_data(self, zone):
+    def get_data(self, zones):
         try:
-
-            print('Veuillez choisir la recurrence des requetes')
+            print('Veuillez choisir la récurrence des requêtes')
             print('-------------------------------------')
             print("1. MINUTES")
             print("2. HEURES")
@@ -138,28 +144,28 @@ class Collector:
             every = int(input('Tous les (CHIFFRES) : '))
 
             while not 1 <= option <= 3:
-                option = int(input('Reessayer:'))
+                option = int(input('Réessayer:'))
 
-            if option == 1:
-                schedule.every(every).minutes.do(self.collect_data, zone)
-            elif option == 2:
-                schedule.every(every).hours.do(self.collect_data, zone)
-            elif option == 3:
-                schedule.every(every).days.do(self.collect_data, zone)
+            for zone in zones:
+                if option == 1:
+                    schedule.every(every).minutes.do(self.collect_data, zone)
+                elif option == 2:
+                    schedule.every(every).hours.do(self.collect_data, zone)
+                elif option == 3:
+                    schedule.every(every).days.do(self.collect_data, zone)
 
-            print("Écriture des données NE PAS FERMER LE PROGRAMME ...")
-            # Start a new thread for the schedule
             schedule_thread = threading.Thread(target=self.run_schedule)
             schedule_thread.start()
+            self.list_threads.append(schedule_thread)
 
-            # Wait for the user to input 'stop' to stop the program
-            while True:
-                cmd = input("Entree 'stop' pour arreter le program: \n")
+            while self.running:
+                cmd = input("Entrez STOP pour arrêter le programme\n")
                 if cmd.strip().lower() == 'stop':
                     self.running = False
                     break
-            print('arret en cours...')
-            schedule_thread.join()
+
+            print('Arrêt en cours...')
+            self.cleanup_threads()
             self.user.logout()
 
         except Exception as e:
@@ -167,14 +173,13 @@ class Collector:
             with open("error_log.txt", "a") as file:
                 file.write(f"{utcmoment_naive} - Erreur dans get_data: {str(e)}\n")
             self.user.logout()
+            exit(1)
 
     def collect_data(self, zone):
         try:
-
             now = time.perf_counter()
             time_to_log = now - self.timeRunning
 
-            #Time to relog
             if time_to_log >= RELOG_TIME:
                 self.login()
                 self.timeRunning = time.perf_counter()
@@ -184,14 +189,13 @@ class Collector:
             utcmoment_naive = datetime.now(pytz.utc)
             utcmoment = utcmoment_naive.replace(tzinfo=pytz.utc)
 
-            #changment pour les îles de la madeleine
             timestamp = utcmoment.astimezone(pytz.timezone('America/Goose_Bay')).strftime("%Y-%m-%d %H:%M:%S")
             date = utcmoment.astimezone(pytz.timezone('America/Goose_Bay')).strftime("%Y-%m-%d")
 
             zone_name = zone_info['Name']
             device_id = zone.device_id
 
-            temperature,humidity = self.get_temperature()
+            temperature, humidity = self.get_temperature()
 
             data = {
                 "device_id": device_id,
@@ -200,21 +204,22 @@ class Collector:
                 "display_temperature": ui_data['DispTemperature'],
                 "display_units": ui_data['DisplayUnits'],
                 "outdoor_humidity": humidity,
-                "outdoor_temperature":temperature,
+                "outdoor_temperature": temperature,
                 "heat_setpoint": ui_data['HeatSetpoint'],
                 "cool_setpoint": ui_data['CoolSetpoint'],
                 "fan_is_running": fan_data['fanIsRunning']
             }
 
-            self.write_in_db(zone_name,device_id,date,data)
-            print(f'Data collected at {timestamp}')
+            self.write_in_db(zone_name, device_id, date, data)
+            print(f'Data collected for {device_id} - {zone_name} at {timestamp}')
 
         except Exception as e:
-            with open("error_log.txt", "w") as file:
+            with open("error_log.txt", "a") as file:
                 file.write(f"Erreur dans collect_data : {str(e)}\n")
             self.user.logout()
+            exit(1)
 
-    def write_in_db(self,zone_name,device_id,date,data):
+    def write_in_db(self, zone_name, device_id, date, data):
         try:
             ref = db.reference(f'/{device_id}/{zone_name}/{date}-THERMOSTAT_DATA')
             ref.push(data)
@@ -222,40 +227,33 @@ class Collector:
             with open("error_log.txt", "a") as file:
                 file.write(f"Erreur dans l'écriture de la base de données : {str(e)}\n")
             self.user.logout()
+            exit(1)
 
     def get_all_zones(self):
         os.system('cls' if os.name == 'nt' else 'clear')
 
-        print('En recherche ...')
-        zones = self.user.get_all_zones()
-
         print('Différentes localisations:')
         print('-------------------------------------')
 
-        for i, zone in enumerate(zones, start=1):
+        for i, zone in enumerate(self.zones, start=1):
             print(f"{i}\tZone ID: {zone.device_id} | Zone Name: {zone.zone_info['Name']}\n")
 
-        choix = int(input('Affichez les infos de la zone # :'))
+        choix = input('Affichez les infos des zones (séparer les numéros par une virgule) :')
+        choix_indices = [int(index.strip()) for index in choix.split(',')]
 
-        while not 1 <= choix <= len(zones):
-            print('Réessayer...')
-            choix = int(input('Affichez les infos de la zone # :'))
+        self.get_data([self.zones[i - 1] for i in choix_indices])
 
-        return zones[choix - 1]
-
+    def cleanup_threads(self):
+        for t in self.list_threads:
+            t.join()
+        #self.list_threads.clear()
 
 if __name__ == "__main__":
-
     try:
-
-        # Création du collecteur de données
         c = Collector()
-        # Retour de la zone qu'on désire obtenir les données
-        zone = c.get_all_zones()
-
-        c.get_data(zone)
-
+        c.get_all_zones()
     except Exception as e:
-        with open(f"error_log.txt", "a") as file:
+        with open("error_log.txt", "a") as file:
             file.write(f"Erreur dans le main : {str(e)}\n")
         print(e)
+        exit(1)
